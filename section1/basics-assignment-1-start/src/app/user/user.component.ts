1f686e4255a665428e68119ac524af3bdbf25067
Mykolas Petrauskas <petrauskas.mykolas@gmail.com>
2017-12-13 02:00:13 +0100

    played with directives

--- section1/basics-assignment-1-start/src/app/user/user.component.ts
+++ section1/basics-assignment-1-start/src/app/user/user.component.ts
@@ -7,11 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
   username:string = '';
+  passwordHidden:boolean = true;
+  log:string[] = [];
+
   constructor() { }
 
   ngOnInit() {
   }
   resetUsername():void{
     this.username = '';
+  }
+
+  displayDetails(event):void{
+    this.passwordHidden = !this.passwordHidden;
+    this.addLog(event);
+
+  }
+
+  addLog(event):void{
+    let logEntree = event.target.localName + " " + event.target.id + " clicked at " + event.timeStamp;
+    this.log.push(logEntree);
+    console.log(this.log);
   }
 }
