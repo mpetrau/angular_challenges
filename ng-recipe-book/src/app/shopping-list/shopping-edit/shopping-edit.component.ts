a1775f4cdd371b95e65a15c55159527835900503
Mykolas Petrauskas <petrauskas.mykolas@gmail.com>
2017-12-29 02:30:08 +0200

    added shopping list Add functionality

--- ng-recipe-book/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ ng-recipe-book/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput') nameInputRef:ElementRef;
+  @ViewChild('amountInput') amountInputRef:ElementRef;
+  @Output() addedIngredient = new EventEmitter<Ingredient>()
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onIngredientAdd(){
+    const newIngredient = new Ingredient (
+      this.nameInputRef.nativeElement.value,
+      this.amountInputRef.nativeElement.value
+    );
+    this.addedIngredient.emit(newIngredient);
   }
 
 }
